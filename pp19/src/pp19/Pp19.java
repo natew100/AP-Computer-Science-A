3af41c9ed72fd2d52056f2e5e941079816c8767f
Nate W <nathanwolf100@gmail.com>
2021-09-28 18:52:13 -0400

    update PP 1.3, 1.4, 1.6, 1.8, 1.9

--- pp19/src/pp19/Pp19.java
+++ pp19/src/pp19/Pp19.java
@@ -2,36 +2,6 @@ package pp19;
 public class Pp19 {
     public static void main(String args[])
     {
-        int numberOfLines, c, k, space=1;      
-        numberOfLines = 5;
-        space = numberOfLines-1;
-        for(k=1; k<=numberOfLines; k++)
-        {
-            for(c=1; c<=space; c++)
-            {
-                System.out.print(" ");
-            }
-            space--;
-            for(c=1; c<=(2*k-1); c++)
-            {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        space = 1;
-        for(k=1; k<=(numberOfLines-1); k++)
-        {
-            for(c=1; c<=space; c++)
-            {
-                System.out.print(" ");
-            }
-            space++;
-            for(c=1; c<=(2*(numberOfLines-k)-1); c++)
-            {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-      
+        System.out.println("     * \n    *** \n   ***** \n  ******* \n ********* \n  ******* \n   ***** \n    *** \n     *");
     }
 }
