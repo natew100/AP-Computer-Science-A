4ed785ebbb3d90455e71e427f35a041c248e117e
Nate W <nathanwolf100@gmail.com>
2021-09-29 21:16:15 -0400

    done

---
+++ pp28/src/pp28/Pp28.java
@@ -0,0 +1,21 @@
+
+package pp28;
+import java.util.Scanner;
+
+
+public class Pp28 {
+
+    public static void main(String[] args) {
+        int seconds, minutes, hours, totalSeconds;
+        Scanner scan = new Scanner (System.in);
+        System.out.print("Enter Seconds: ");
+        seconds = scan.nextInt();
+        System.out.print("Enter Minutes: ");
+        minutes = scan.nextInt();
+        System.out.print("Enter Hours: ");
+        hours = scan.nextInt();
+        totalSeconds = 3600*hours+60*minutes+seconds;
+        System.out.println("total Number of Seconds: " + totalSeconds);
+    }
+    
+}
