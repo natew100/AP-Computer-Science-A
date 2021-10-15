62d8061af0fd64f23adbfb096c82fe7ee8ed5705
Nathan Wolf <3009496@chclc.org>
2021-10-15 10:26:44 -0400

    jghj

--- pp2.5/src/pp2/pkg5/Pp25.java
+++ pp2.5/src/pp2/pkg5/Pp25.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Pp25 {
     public static void main(String[] args) {
-        float temperature, finalTemp;
+        double temperature, finalTemp;
         Scanner in = new Scanner(System.in);
         System.out.print("Enter temperature in Fahrenheit: ");
-        temperature = in.nextInt();
+        temperature = in.nextDouble();
         finalTemp = ((temperature - 32)*5)/9;
         System.out.println("temperature in Celsius = " + finalTemp);
     }
