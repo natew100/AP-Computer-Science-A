ea3c8683f006f25a2e0ee88f2c01254addf02858
Nate W <nathanwolf100@gmail.com>
2021-11-10 22:35:12 -0500

    54

---
+++ pp5.3/src/pp5/pkg3/Pp53.java
@@ -0,0 +1,21 @@
+package pp5.pkg3;
+import java.util.Scanner;
+
+public class Pp53 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int n = in.nextInt(), d;
+        while (n > 0) {
+            d = n % 10;
+            if (d == 0) {
+                System.out.println(d + " is zero");
+            } else if (d % 2 == 0) {
+                System.out.println(d + " is even");
+            } else {
+                System.out.println(d + " is odd");
+            }
+            n /= 10;
+        }
+    }
+}
