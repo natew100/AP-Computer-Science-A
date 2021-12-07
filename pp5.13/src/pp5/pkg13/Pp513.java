f18fecb98bf7acd46a80849b5c0554326b4ad857
Nate W <54341514+natew100@users.noreply.github.com>
2021-12-07 10:24:50 -0500

    .

--- pp5.13/src/pp5/pkg13/Pp513.java
+++ pp5.13/src/pp5/pkg13/Pp513.java
@@ -6,9 +6,11 @@ public class Pp513 {
     public static void main(String args[]) {
 
         try {
+            // init scanner for files
             Scanner fileScan1 = new Scanner(new File("C:\\Users\\3009496\\Desktop\\AP-Computer-Science-A\\pp5.13\\src\\pp5\\pkg13\\file1"));
             Scanner fileScan2 = new Scanner(new File("C:\\Users\\3009496\\Desktop\\AP-Computer-Science-A\\pp5.13\\src\\pp5\\pkg13\\file2"));
             while (fileScan1.hasNext() && fileScan2.hasNext()) {
+                // while loop to scan each line
                 String file1Lines = fileScan1.nextLine();
                 String file2Lines = fileScan2.nextLine();    
                 if (!file1Lines.equals(file2Lines)) {
